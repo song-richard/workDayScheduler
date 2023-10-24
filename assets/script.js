c06f39e8172cbcacae9eaca60c100b69f145ef43
Richard Song <rich@Richards-MacBook-Pro.local>
2023-10-23 19:17:46 -0700

    Fixed html structure issue

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html. -Done
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this? -Done
-  //
-  // TODO: Add code to display the current date in the header of the page. -Done
-
-});
-
 // Show Current Date/Time
 function showDate() {
   const currentDate = dayjs();
@@ -38,7 +13,8 @@ function updateTimeBlockClasses() {
 
   const timeBlocks = document.querySelectorAll('.time-block');
   timeBlocks.forEach((block) => {
-    const blockHour = parseInt(block.id.split('-')[1], 10); // Extract the hour from the block's id
+    //Extracts hour from id
+    const blockHour = parseInt(block.id.split('-')[1], 10); 
 
     if (blockHour < currentHour) {
       block.classList.remove('present', 'future');
@@ -57,7 +33,7 @@ function updateTimeBlockClasses() {
 showDate()
 //Updates Time Blocks
 updateTimeBlockClasses()
-//Set Interval to refresh
+//Set Interval to refresh time blocks
 setInterval(updateTimeBlockClasses, 1000);
 
 //Ensures HTML is fully loaded before running the code below
